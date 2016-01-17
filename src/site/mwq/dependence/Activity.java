6dc363a58a1877227206492f31b9e687ca7581b2
lazycoder <qiuweimin@126.com>
2016-01-17 17:37:19 +0800

    test

--- src/site/mwq/dependence/Activity.java
+++ src/site/mwq/dependence/Activity.java
@@ -2,6 +2,11 @@ package site.mwq.dependence;
 
 import java.util.ArrayList;
 
+/**
+ * 一次活动表示一次迁移
+ * @author Email:qiuweimin@126.com
+ * @date 2016年1月17日
+ */
 public class Activity {
 	public String label;	//活动标签,如A1
 	public double duration;		//活动持续时间
